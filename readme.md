9cf035b29ad46979b9d57be935a303a0e7ee1c1e
Chandrima-Barua <chandrimabarua62065@gmail.com>
2021-12-29 15:13:02 +0100

    output folder is removed for testing purpose

---
+++ readme.md
@@ -0,0 +1,5 @@
+Please follow these steps to run the spark job
+1. sudo service docker start
+2. Go to Pyspark shell: pyspark
+3. Run the code from word_count.py inside pyshark shell
+4. For submitting the spark job run :  spark-submit --master local word_count.py
--- word_count.py
+++ word_count.py
@@ -1,16 +1,36 @@
 from pyspark import SparkContext
+# # Create SparkSession and sparkcontext
+# from pyspark.sql import SparkSession
+#
+# def main():
+#    spark = SparkSession.builder\
+#                        .master("local")\
+#                        .appName('SparkWordCount')\
+#                        .getOrCreate()
+#    sc=spark.sparkContext
+# #    sc = SparkContext(appName='SparkWordCount')
+#    input_file = sc.textFile('datafile.txt')
+#    counts = input_file.flatMap(lambda line: line.split()) \
+#                      .map(lambda word: (word, 1)) \
+#                      .reduceByKey(lambda a, b: a + b)
+#    counts.saveAsTextFile('output')
+#
+#    sc.stop()
+#
+# if __name__ == '__main__':
+#    main()
 
-def main():
 
-   sc = SparkContext(appName='SparkWordCount')
+# for pyspark
+sc = SparkContext()
+# # The WordCounts Spark program
+textFile = sc.textFile("datafile.txt")
+wordCounts = textFile.flatMap(lambda line: line.split()).map(lambda word:     (word, 1)).reduceByKey(lambda a, b: a+b)
+wordCounts.saveAsTextFile('output')
 
-   input_file = sc.textFile('datafile.txt')
-   counts = input_file.flatMap(lambda line: line.split()) \
-                     .map(lambda word: (word, 1)) \
-                     .reduceByKey(lambda a, b: a + b)
-   counts.saveAsTextFile('output')
 
-   sc.stop()
 
-if __name__ == '__main__':
-   main()
+
+
+
+
